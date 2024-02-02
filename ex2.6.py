37d2710da45d0b09d18c0bb9a989a20c8069a83e
Mariem <Mariemouertatani2001@gmail.com>
2024-02-01 23:11:30 -0700

    final edit

--- ex2.6.py
+++ ex2.6.py
@@ -2,13 +2,13 @@ import json
 import timeit
 
 #factorial python funtion with integer n
-def factorial0(n):
+def fact(n):
     if n == 0:
         return 1
     else:
-        return n * factorial0(n - 1)
+        return n * fact(n - 1)
 # utilizing timeit module  to time the execution of 100 times of factorial(100)
-total_time0 = timeit.timeit(stmt="factorial0(100)", setup="from __main__ import factorial0", number=10000)
+total_time0 = timeit.timeit(stmt="fact(100)", setup="from __main__ import fact", number=10000)
 
 #printing out the results from the timeit module
 print("factorialorial (10000 runs) " + str(total_time0))
@@ -20,19 +20,21 @@ def factorial1(n):
         result = result * i
     return result
 
-# utilizing timeit module  to time the execution of 100 times of factorial(100)
+#utilizing timeit module  to time the execution of 100 times of factorial(100)
 total_time_1 = timeit.timeit(stmt="factorial1(100)", setup="from __main__ import factorial1", number=1000)
 
 #printing out the results from the timeit module
 print("for loop method (1000 runs) " + str(total_time_1))
 
 #factorial python funtion with integer n using a list comprehension
-# using list comprehension was very diffucult for me so i used Chatgpt to help me come up with the syntax for 
-#line 32 and 33 since I couldnt find information regaurding this in the notes.
+
+#AI use declaration (34-35)
+#using list comprehension was very diffucult for me so i used Chatgpt to help me come up with the syntax for 
+#line 34 and 35 since I couldnt find information regaurding this in the notes.
 def factorial2(n):
     return 1 if n == 0 else n * factorial2(n - 1)
 
-# utilizing timeit module  to time the execution of 100 times of factorial(100)
+#utilizing timeit module  to time the execution of 100 times of factorial(100)
 total_time_2 = timeit.timeit(stmt="factorial2(100)", setup="from __main__ import factorial2", number=1000)
 
 #printing out the results from the timeit module
